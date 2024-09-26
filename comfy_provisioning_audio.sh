89f8ada15aa45d15173ab5ebc9ed003f840795c2
rubypazhung <2001ruby@gmail.com>
2024-09-26 15:41:36 +0900

    Update comfy_provisioning_audio.sh

--- comfy_provisioning_audio.sh
+++ comfy_provisioning_audio.sh
@@ -34,6 +34,7 @@ CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/125771?type=Model&format=SafeTensor&size=pruned&fp=fp16"
     "https://civitai.com/api/download/models/128713?type=Model&format=SafeTensor&size=pruned&fp=fp16"
     "https://huggingface.co/stabilityai/stable-audio-open-1.0/resolve/main/model.safetensors"
+    #"https://civitai.com/api/download/models/143906?type=Model&format=SafeTensor&size=pruned&fp=fp16"
 )
 
 UNET_MODELS=(
